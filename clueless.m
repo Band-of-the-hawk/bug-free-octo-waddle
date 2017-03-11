6f1800a9b4b46bfbb39649178b2c3b1b349f947d
FRANKENSTEIN\ThomasSTodal <thomatod@stud.ntnu.no>
2017-03-11 18:35:42 +0100

    no message

--- clueless.m
+++ clueless.m
@@ -6,40 +6,52 @@ clc;
 fileID = fopen('log_files\rabbit-log-file.csv');
 FD_data = textscan(fileID,'%d%d%s','Delimiter', ',');
 fclose(fileID);
-rabbitAge = FD_data{1,1};
-rabbitStep = FD_data{1,2};
+rabbitInfo = FD_data{1,1};
+rabbitInfo = [ rabbitInfo FD_data{1,2} ];
+rabbitAge = rabbitInfo(1,1);
+rabbitStep = rabbitInfo(1,2);
 clear('fileID','FD_data');
 
 % Åpner og henter ut data fra fox-log-file.csv
 fileID = fopen('log_files\fox-log-file.csv');
 FD_data = textscan(fileID,'%d%d%s','Delimiter', ',');
 fclose(fileID);
-foxAge = FD_data{1,1};
-foxStep = FD_data{1,2};
 foxInfo = FD_data{1,1};
 foxInfo = [ foxInfo FD_data{1,2} ];
+foxAge = foxInfo(:,1);
+foxStep = foxInfo(:,2);
 clear('fileID','FD_data');
-
-%foxInfo(:,{'Age','Step'})
-
-%asdf = foxInfo(foxInfo{1,2}==4)
-
-foxSize = size(foxInfo);
-foxSize = foxSize(1);
 
 % Åpner og henter ut data fra wolf-log-file.csv
 fileID = fopen('log_files\wolf-log-file.csv');
 FD_data = textscan(fileID,'%d%d%s','Delimiter', ',');
 fclose(fileID);
-wolfAge = FD_data{1,1};
-wolfStep = FD_data{1,2};
+wolfInfo = FD_data{1,1};
+wolfInfo = [ wolfInfo FD_data{1,2} ];
+wolfAge = wolfInfo(1,1);
+wolfStep = wolfInfo(1,2);
 clear('fileID','FD_data');
 
 % Åpner og henter ut data fra population-log-file.csv
 fileID = fopen('log_files\population-log-file.csv');
 FD_data = textscan(fileID,'%d%d%d','Delimiter', ',');
 fclose(fileID);
-rabbitPop = FD_data{1,1};
-foxPop = FD_data{1,2};
-wolfPop = FD_data{1,3};
+popInfo = FD_data{1,1};
+popInfo = [ popInfo FD_data{1,2} ];
+popInfo = [ popInfo FD_data{1,3} ];
+rabbitPop = popInfo(1,1);
+foxPop = popInfo(1,2);
+wolfPop = popInfo(1,3);
 clear('fileID','FD_data');
+
+% Lat eksperimenteringa byrje!
+
+
+foxSize = size(foxInfo);
+foxSize = foxSize(1);
+foxAvgAge = zeros();
+for 1 = 0:foxSize
+    foxAvgStep = zeros();
+    foxThisStep = 1;
+    if
+end
