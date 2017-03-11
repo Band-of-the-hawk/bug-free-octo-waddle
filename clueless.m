00d765c86f54d9299faa0e20099736524a601522
FRANKENSTEIN\ThomasSTodal <thomatod@stud.ntnu.no>
2017-03-11 18:02:59 +0100

    literally no clue

---
+++ clueless.m
@@ -0,0 +1,43 @@
+%% Oppgave 2
+clear;
+clc;
+
+% Åpner og henter ut data fra rabbit-log-file.csv
+fileID = fopen('log_files\rabbit-log-file.csv');
+FD_data = textscan(fileID,'%d%d%s','Delimiter', ',');
+fclose(fileID);
+rabbitAge = FD_data{1,1};
+rabbitStep = FD_data{1,2};
+clear('fileID','FD_data');
+
+% Åpner og henter ut data fra fox-log-file.csv
+fileID = fopen('log_files\fox-log-file.csv');
+FD_data = textscan(fileID,'%d%d%s','Delimiter', ',');
+fclose(fileID);
+foxAge = FD_data{1,1};
+foxStep = FD_data{1,2};
+foxInfo = FD_data{1,1};
+foxInfo = [ foxInfo FD_data{1,2} ];
+
+foxInfo(:,{'Age','Step'})
+
+asdf = foxInfo(foxInfo.Step==4)
+
+clear('fileID','FD_data');
+
+% Åpner og henter ut data fra wolf-log-file.csv
+fileID = fopen('log_files\wolf-log-file.csv');
+FD_data = textscan(fileID,'%d%d%s','Delimiter', ',');
+fclose(fileID);
+wolfAge = FD_data{1,1};
+wolfStep = FD_data{1,2};
+clear('fileID','FD_data');
+
+% Åpner og henter ut data fra population-log-file.csv
+fileID = fopen('log_files\population-log-file.csv');
+FD_data = textscan(fileID,'%d%d%d','Delimiter', ',');
+fclose(fileID);
+rabbitPop = FD_data{1,1};
+foxPop = FD_data{1,2};
+wolfPop = FD_data{1,3};
+clear('fileID','FD_data');
